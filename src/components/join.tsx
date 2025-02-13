05a54133e66b0aafc8cf6eccdf6ab31c47cc5617
LucasSovre <lucas.sovre@proton.me>
2025-02-13 17:09:07 -0500

    first version

--- src/components/join.tsx
+++ src/components/join.tsx
@@ -1,10 +1,13 @@
 import toast from "react-hot-toast";
 import {useState} from "react";
 import posthog from "posthog-js";
+import {useFeatureFlagVariantKey} from "posthog-js/react";
+import { clsx } from "clsx";
 
 export default function Join(){
 
     const [email, setEmail] = useState("")
+    const variant = useFeatureFlagVariantKey('slate-blue-join-button')
 
     async function req(){
         const resp = await fetch(`https://n8n.sovre.fr/webhook/7b739d30-dfad-485a-a62a-a86e982e5f39?email=${email}`)
@@ -30,10 +33,17 @@ export default function Join(){
 
     return (
         <div className="flex flex-col md:flex-row w-full items-center justify-center gap-2 py-10 pb-20">
-                    <span className="border-zinc-500 border-2 rounded-lg py-2 px-5 flex w-xs">
+                    <span className={clsx(
+                        "border-2 rounded-lg py-2 px-5 flex w-xs",
+                                variant === "control" ? "border-zinc-500" : "border-blue-500"
+                        )}>
                         <input value={email} onChange={(e)=>(setEmail(e.target.value))} placeholder="your@email.com" type="text" className="outline-0 text-white w-full"/>
                     </span>
-            <button onClick={()=>{handleJoin()}} className="text-white bg-gradient-to-r from-slate-500 to-slate-700 rounded-lg py-2 px-5 transition-all hover:scale-105">
+            <button onClick={()=>{handleJoin()}} className={clsx(
+                "text-white bg-gradient-to-r rounded-lg py-2 px-5 transition-all hover:scale-105",
+                variant === "control" ? "from-slate-500 to-slate-700" : "from-blue-500 to-blue-700"
+            )}
+                >
                 Join waitlist !
             </button>
         </div>
--- src/first.tsx
+++ src/first.tsx
@@ -2,15 +2,38 @@ import {Spotlight} from "./components/ui/spotlight-new.tsx";
 import playgroundDevelopped from "./assets/playground-developped.png";
 import logo from "./assets/logo.svg"
 import Join from "./components/join.tsx";
+import {clsx} from "clsx";
+import {useFeatureFlagVariantKey} from "posthog-js/react";
 
 export default function First(){
+
+    const variant = useFeatureFlagVariantKey('slate-blue-join-button')
+    const txtVariant = useFeatureFlagVariantKey('heroe-title-content')
+
+    function getText() {
+        switch (txtVariant) {
+            case "control":
+                return "Deploy apps and manage your server easier than ever";
+            case "option1":
+                return "Deploy Apps Without Worrying About Docker or Kubernetes—Just Click & Go";
+            case "option2":
+                return "Your time is valuable. Deploy your apps the smart way – simple, fast, and open-source"
+            default:
+                return "Deploy apps and manage your server easier than ever";
+        }
+    }
+
     return(
         <>
             <nav className="text-white max-w-7xl w-full flex flex-row justify-between items-center mb-20 mt-5 px-10">
-                <img src={logo} className="w-40" />
+                <img alt="logo" src={logo} className="w-40" />
                 <div className="flex flex-row gap-5 justify-center items-center">
+                    <a href="https://github.com/composecraft/next">Github</a>
                     <a href="mailto:contact@entrecompetents.fr">contact</a>
-                    <a href="/#join" className="text-white bg-gradient-to-r from-slate-500 to-slate-700 rounded-lg py-2 px-5">
+                    <a href="/#join" className={clsx(
+                        "text-white bg-gradient-to-r rounded-lg py-2 px-5 transition-all hover:scale-105",
+                        variant === "control" ? "from-slate-500 to-slate-700" : "from-blue-500 to-blue-700"
+                    )}>
                         Join waitlist !
                     </a>
                 </div>
@@ -18,10 +41,10 @@ export default function First(){
             <Spotlight/>
             <div className="flex flex-col p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
                 <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-                    Deploy and manage your server easier than ever
+                    {getText()}
                 </h1>
                 <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-                    Do not waste time learning docker, kubernetes or whatever, deploy apps now, for free.
+                    Do not waste time learning docker, kubernetes or whatever, deploy apps now, for free. It's Open source.
                 </p>
             </div>
             <span id="join"></span>
